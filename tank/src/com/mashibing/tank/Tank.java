32a05481fb887e668c8e33ce0336ac4c4db032fe
xiangzhigu <gxz16120341222@163.com>
2020-12-07 22:44:33 +0800

    区分敌我坦克

--- tank/src/com/mashibing/tank/Tank.java
+++ tank/src/com/mashibing/tank/Tank.java
@@ -11,8 +11,8 @@ public class Tank {
 
     private Random random  = new Random();
 
-    public static int WIDTH = ResourceMgr.tankL.getWidth();
-    public static int HEIGHT = ResourceMgr.tankL.getHeight();
+    public static int WIDTH = ResourceMgr.goodTankU.getWidth();
+    public static int HEIGHT = ResourceMgr.goodTankU.getHeight();
 
     private boolean moving = true;
     private boolean living = true;
@@ -69,18 +69,21 @@ public class Tank {
 
     public void paint(Graphics g){
         if(!living) tankFrame.tanks.remove(this);
+
+
+
         switch (dir){
             case LEFT:
-                g.drawImage(ResourceMgr.tankL,x,y,null);
+                g.drawImage(this.group ==Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL,x,y,null);
                 break;
             case UP:
-                g.drawImage(ResourceMgr.tankU,x,y,null);
+                g.drawImage(this.group ==Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU,x,y,null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR,x,y,null);
+                g.drawImage(this.group ==Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR,x,y,null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD,x,y,null);
+                g.drawImage(this.group ==Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD,x,y,null);
                 break;
         }
 //        g.setColor(Color.YELLOW);
